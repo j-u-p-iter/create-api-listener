059f4481f3cc55e8e3537964c00d3f0d44ae8ae5
kirill <pn79122821224@gmail.com>
2018-11-18 15:41:13 +0100

    test(create-api-listener): add tests

--- src/__tests__/create-api-listener.test.ts
+++ src/__tests__/create-api-listener.test.ts
@@ -1,7 +1,40 @@
-import { createApiListener } from '../create-api-listener';
+import { createApiListener } from '../';
 
 describe('createApiListener', () => {
-  it('should work great', () => {
-    expect(true).toBe(true);
-  })
+  let response: { [key: string]: number };
+  let apiCall: () => Promise<any>;
+
+  beforeAll(() => {
+    response = { data: 5 };
+
+    apiCall = () => new Promise(resolve => {
+      setTimeout(() => resolve(response), 2000);
+    });
+  });
+
+  describe('when we get necessary response', () => {
+    it('should be resolved with response', async () => {
+      const callback = (response: any) => response.data === 5;
+
+      const apiListener = createApiListener(apiCall, callback);
+
+      const result = await apiListener();
+      const expected = response;
+
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('when we don`t get necessary response', () => {
+    it('should be resolved with last response after time runs out', async () => {
+      const callback = (response: any) => response.data === 4;
+
+      const apiListener = createApiListener(apiCall, callback, 0, 3000);
+
+      const result = await apiListener();
+      const expected = response;
+
+      expect(result).toBe(expected);
+    });
+  });
 });
